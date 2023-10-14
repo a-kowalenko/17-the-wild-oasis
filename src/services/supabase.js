2d348f31096f8d9fc1f68b4c91577b2d7fd9c684
Andreas Kowalenko <andreas.kowalenko@hotmail.com>
2023-10-14 16:30:44 +0200

    supabase init

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://uvisdzgjedmxzbhgdkxo.supabase.co";
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2aXNkemdqZWRteHpiaGdka3hvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcyODE5ODUsImV4cCI6MjAxMjg1Nzk4NX0.-crNBTygfh1jJoUV_yBbOn-FsqzutOuCYv06GRQecks";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;

eaf5311687c95a96e0a29a376df322118c7a5b27
Andreas Kowalenko <kowalenko@eddi24.de>
2023-10-16 16:42:11 +0200

    cabin filter and sort features

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,13 +3,36 @@ import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
     const { isLoading, cabins } = useCabins();
+    const [searchParams] = useSearchParams();
 
     if (isLoading) {
         return <Spinner />;
     }
+
+    // 1. FILTER
+    const filterValue = searchParams.get("discount") || "all";
+    console.log(filterValue);
+
+    let filteredCabins;
+    if (filterValue === "all") {
+        filteredCabins = cabins;
+    } else if (filterValue === "no-discount") {
+        filteredCabins = cabins.filter((cabin) => !cabin.discount);
+    } else if (filterValue === "with-discount") {
+        filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+    }
+
+    // 2. SORT
+    const sortBy = searchParams.get("sortBy") || "startDate-asc";
+    const [field, direction] = sortBy.split("-");
+    const modifier = direction === "asc" ? 1 : -1;
+    const sortedCabins = filteredCabins.sort(
+        (a, b) => (a[field] - b[field]) * modifier
+    );
 
     return (
         <Menus>
@@ -23,7 +46,7 @@ function CabinTable() {
                     <div></div>
                 </Table.Header>
                 <Table.Body
-                    data={cabins}
+                    data={sortedCabins}
                     render={(cabin) => (
                         <CabinRow cabin={cabin} key={cabin.id} />
                     )}
